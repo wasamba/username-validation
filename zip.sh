75d48bcf147504e0c23e4b55bf388a876d1b82f5
Alessandra Figueiredo <afigueiredo@chapman.edu>
2024-10-20 22:49:10 -0700

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -3,11 +3,13 @@
 echo "Enter a valid username: "
 read -r username
 
-while echo "$username" | grep -E -v "^[a-z][a-z0-9_]{2,11}$ ]];" > /dev/null 2>&1
+# Loop until a valid username is entered
+while ! echo "$username" | grep -E "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "Invalid username! Username must start with a lowercase a lowercase letter, can only contain lowercase letters, digits, or underscores, and must be between 3 and 12 characters long."
+    echo "Invalid username! Username must start with a lowercase letter, can only contain lowercase letters, digits, or underscores, and must be between 3 and 12 characters long."
     echo "Enter a potential username: "	
-	read -r username
+    read -r username
 done
-echo "Thank you"
+
+echo "Thank you!"
 
